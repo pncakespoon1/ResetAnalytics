83e1360966b795fc011a2c1fd35c2f981ad431af
Specnr <specnrr@gmail.com>
2022-03-24 00:47:40 -0400

    added session stats

--- pages/sheet/[sheetId].jsx
+++ pages/sheet/[sheetId].jsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { Spinner} from 'react-bootstrap'
+import { Spinner, Tabs, Tab } from 'react-bootstrap'
 import useSWR from "swr"
 import Stats from '../../components/user/Stats';
 
@@ -13,12 +13,23 @@ const UserView = () => {
   if (error) return "An error has occurred."
   if (!data) return <Spinner animation="border" style={{minHeight: "2em", minWidth: "2em", fontSize: "2em"}} />
   if (!data.success) return <h1>Invalid sheet :(</h1>
-  console.log(data)
   return (
-    <>
-      <h1 className="display-2">Lifetime Stats</h1>
-      <Stats data={data.overall} />
-    </>
+    <Tabs transition={false}>
+      <Tab eventKey="lifetime" title="Lifetime">
+        <h1 className="display-2">Lifetime Stats</h1>
+        <Stats data={data.overall} />
+      </Tab>
+      {
+        data.session.map((session, idx) => {
+          return (
+            <Tab key={idx} eventKey={session.time} title={`Session #${data.session.length - idx}`}>
+              <h1 className="display-2">Lifetime Stats</h1>
+              <Stats data={session.ops} />
+            </Tab>
+          )
+        })
+      }
+    </Tabs>
   )
 }
 
