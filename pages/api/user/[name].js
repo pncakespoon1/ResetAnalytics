6f4dbbd6b1dc242dfde1292a1587cfa2190d86c0
Specnr <specnrr@gmail.com>
2022-06-03 17:39:14 -0400

    added name to sheet id

--- pages/api/user/[name].js
+++ pages/api/user/[name].js
@@ -1,6 +1,23 @@
-import { checkNameInJson } from "../../../public/helpers/fetchFromJson"
+const reader = require("g-sheets-api");
 
 export default async function handler(req, res) {
-  const nameData = await checkNameInJson(req.query.name)
-  res.status(nameData.sheetId.length > 0 ? 200 : 400).json(nameData)
+  const name = req.query.name.toLowerCase()
+  const readerOptions = {
+    apiKey: process.env.SHEETS_API_KEY,
+    sheetId: process.env.NAME_KEY_SHEET,
+    returnAllResults: false,
+    sheetName: 'Data'
+  };
+  return new Promise(resolve => {
+    reader(readerOptions, data => {
+      const userRow = data.filter(row => row["Username"] === name)
+      console.log(userRow)
+      if (userRow.length != 1) {
+        res.status(404).json({success: false})
+        return resolve()
+      } else {
+        res.status(200).json({success: true, sheetId: userRow[0]["SheetId"]})
+      }
+    })
+  })
 }
