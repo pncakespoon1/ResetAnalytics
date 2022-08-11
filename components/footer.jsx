f56ecf9963097c101c166bb59691f0e4493f3a2c
Specnr <specnrr@gmail.com>
2022-08-11 13:03:26 -0400

    spelling

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => (
   <>
     <p className="mb-0" style={{bottom: "0", left: "0", right: "0"}}>
-      Compatable only with my <a href="https://github.com/Specnr/ResetTracker/releases/latest" style={{textDecoration: "none"}}>Reset Tracker</a>
-      </p>
+      Compatible only with my <a href="https://github.com/Specnr/ResetTracker/releases/latest" style={{textDecoration: "none"}}>Reset Tracker</a>
+    </p>
     <p className="mb-0" style={{bottom: "0", left: "0", right: "0"}}>
       Made with <span style={{color: "red"}}>❤</span> by Specnr
     </p>
