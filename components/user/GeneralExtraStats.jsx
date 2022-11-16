3a38d2d501af6d8ae71bedc53fc0247cb0b57bfd
Specnr <specnrr@gmail.com>
2022-11-16 15:27:24 -0500

    added back bph

--- components/user/GeneralExtraStats.jsx
+++ components/user/GeneralExtraStats.jsx
@@ -7,6 +7,7 @@ const GeneralExtraStats = ({ data, isSess }) => {
       <thead>
         <tr>
           <th>Nethers/Hr</th>
+          <th>Blinds/Hr</th>
           <th>Resets</th>
           <th>Playtime</th>
           <th>Time Per Played</th>
@@ -17,6 +18,7 @@ const GeneralExtraStats = ({ data, isSess }) => {
       <tbody style={{fontFamily: "Roboto", fontSize: "1em"}}>
         <tr>
           <td>{ roundToPerc(data.nph) }</td>
+          <td>{ roundToPerc(data.bph) }</td>
           <td>{ data.rc }</td>
           <td>{ msToStr(data.tp) }</td>
           <td>{ msToStr(data.tp / data.pc, true) }</td>
