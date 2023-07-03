86f28ae2fb8568aa7a096d81eecbb378aa121de7
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2023-07-03 14:25:48 -0400

    reorganizing components w/ tabs

--- components/user/Stats.jsx
+++ components/user/Stats.jsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap"
 import { timelines } from "../../public/helpers/frontend"
+import { Tab, Tabs } from 'react-bootstrap'
 import CollapsibleContainer from "./custom/CollapsibleContainer"
 import SplitInfo from "./figures/SplitInfo"
 import TimelineTable from "./figures/TimelineTable"
@@ -15,54 +16,96 @@ import NetherTree from "./figures/NetherTree"
 const Stats = ({ data, isSess }) => {
   return (
     <>
-      <TimelineTable data={data} />
-      <GeneralExtraStats data={data} isSess={isSess} />
-      <EnterTypeGraph data={data} />
-      <BiomeTypeGraph data={data} />
-      <IronSourceGraph data={data} />
-      <Row style={{ width: "100%" }}>
-        <PlaytimePieChart data={data} />
-      </Row>
-      <TwoWayEnterTable data={data} />
-      <CollapsibleContainer header={<h1>Overworld</h1>}>
-        {data.tl.slice(0, 4).map((val, idx) =>
+      <Tabs transition={false}>
+        <Tab eventKey="overview" title="Overview">
           <Row style={{ width: "100%" }}>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitInfo splitData={val} splitName={timelines[idx]} />
-            </Col>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitHistogram distData={val.cDist} />
+            <TimelineTable data={data} />
+          </Row>
+          <Row style={{ width: "100%" }}>
+            <GeneralExtraStats data={data} isSess={isSess} />
+          </Row>
+          <Row style={{ width: "100%" }}>
+            <Col style={{ height: "300px" }}>
+              <PlaytimePieChart data={data} />
             </Col>
           </Row>
-        )}
-      </CollapsibleContainer>
-      <CollapsibleContainer header={<h1>Nether</h1>}>
-        <Row style={{ width: "100%" }}>
-          <NetherTree netherTreeData={data.ntd} />
-        </Row>
-        {data.tl.slice(4, 7).map((val, idx) =>
-          <Row style={{ width: "100%" }}>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitInfo splitData={val} splitName={timelines[idx + 4]} />
-            </Col>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitHistogram distData={val.cDist} />
-            </Col>
-          </Row>
-        )}
-      </CollapsibleContainer>
-      <CollapsibleContainer header={<h1>Endgame</h1>}>
-        {data.tl.slice(7, 9).map((val, idx) =>
-          <Row style={{ width: "100%" }}>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitInfo splitData={val} splitName={timelines[idx + 7]} />
-            </Col>
-            <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
-              <SplitHistogram distData={val.cDist} />
-            </Col>
-          </Row>
-        )}
-      </CollapsibleContainer>
+        </Tab>
+        <Tab eventKey="overworld" title="Overworld">
+          {
+            (
+              data.tl[3].total !== 0 ? (
+                <div>
+                  <EnterTypeGraph data={data} />
+                  <BiomeTypeGraph data={data} />
+                  <IronSourceGraph data={data} />
+                  <TwoWayEnterTable data={data} />
+                </div>
+              ) : null
+            )
+          }
+          {
+            data.tl.slice(0, 4).map((val, idx) =>
+              <Row style={{ width: "100%" }}>
+                <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                  <SplitInfo splitData={val} splitName={timelines[idx]} />
+                </Col>
+                <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                  <SplitHistogram distData={val.cDist} />
+                </Col>
+              </Row>
+            )
+          }
+        </Tab>
+        <Tab eventKey="nether" title="Nether">
+          {
+            (
+              data.tl[3].total !== 0 ? (
+                <div>
+                  {
+                    <Row style={{ width: "100%" }}>
+                      <NetherTree netherTreeData={data.ntd} />
+                    </Row>
+                  }
+                  {
+                    data.tl.slice(4, 7).map((val, idx) =>
+                      <Row style={{ width: "100%" }}>
+                        <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                          <SplitInfo splitData={val} splitName={timelines[idx + 4]} />
+                        </Col>
+                        <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                          <SplitHistogram distData={val.cDist} />
+                        </Col>
+                      </Row>
+                    )
+                  }
+                </div>
+              ) : null
+            )
+          }
+        </Tab>
+        <Tab eventKey="endgame" title="End-game">
+          {
+            (
+              data.tl[6].total !== 0 ? (
+                <div>
+                  {
+                    data.tl.slice(7, 9).map((val, idx) =>
+                      <Row style={{ width: "100%" }}>
+                        <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                          <SplitInfo splitData={val} splitName={timelines[idx + 7]} />
+                        </Col>
+                        <Col style={{ height: "300px" }} className="d-flex flex-column col-md-6 col-sm-12">
+                          <SplitHistogram distData={val.cDist} />
+                        </Col>
+                      </Row>
+                    )
+                  }
+                </div>
+              ) : null
+            )
+          }
+        </Tab>
+      </Tabs>
     </>
   )
 }
