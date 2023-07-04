198fc0713a856365789e9c8f919ccfae0ff1f193
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2023-07-04 16:43:17 -0400

    histogram made tighter

--- components/user/figures/SplitHistogram.jsx
+++ components/user/figures/SplitHistogram.jsx
@@ -10,10 +10,10 @@ const SplitHistogram = ({ data1 }) => {
     <>
       <h1>Split Distribution</h1>
       <ResponsiveContainer>
-        <LineChart width={500} height={250} data={distData} margin={{ top: 10, right: 10, bottom: 70, left: 5 }}>
+        <LineChart width={500} height={250} data={distData} margin={{ top: 10, right: 10, bottom: 40, left: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="time" tick={{ angle: 80, textAnchor: "start", dx: 0 }} stroke="#b2b2b2" tickFormatter={value => msToStr(value, false)} interval={Math.floor(distData.length / 8) - 1} >
-            <Label value="Time" offset={-45} position="insideBottom" style={{ textAnchor: 'middle', fill: '#b2b2b2' }} />
+            <Label value="Time" offset={-25} position="insideBottom" style={{ textAnchor: 'middle', fill: '#b2b2b2' }} />
           </XAxis>
           <YAxis stroke="#b2b2b2" ticks={yTicks} domain={[yTicks[0], yTicks[yTicks.length - 1]]} >
             <Label value="Cumulative Count" offset={0} position="insideLeft" angle={-90} style={{ textAnchor: 'middle', fill: '#b2b2b2' }} />
